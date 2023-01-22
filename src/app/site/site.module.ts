7d754ad4678686f2f7cb8d179bf86d5208481ca5
CaptainSP <19982002ke@gmail.com>
2023-01-22 12:30:15 +0300

    Ana sayfa

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomepageComponent } from './homepage/homepage.component';
-import { AppUiModule } from '@common/app-ui/app-ui.module';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { GuestHomeComponent } from './main-home/guest-home/guest-home.component';
+import { UserHomeComponent } from './main-home/user-home/user-home.component';
+import { SiteRoutingModule } from './site.routing.module';
+import { AppCommonModule } from '@common/common.module';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [
+    MainHomeComponent,
+    GuestHomeComponent,
+    UserHomeComponent,
+
+  ],
   imports: [
     CommonModule,
-    AppUiModule,
-    MatCardModule,
-    MatListModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonToggleModule
-  ],
-  exports: [HomepageComponent],
+    SiteRoutingModule,
+    AppCommonModule,
+    MatButtonModule,
+    MatIconModule
+  ]
 })
-export class SiteModule {}
+export class SiteModule { }
