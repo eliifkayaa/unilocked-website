ed85e668030b02a88887806344ac24b99312b619
CaptainSP <19982002ke@gmail.com>
2023-04-03 20:21:25 +0300

    User Profile

--- src/app/site/profiles/user-profile/user-profile.component.ts
+++ src/app/site/profiles/user-profile/user-profile.component.ts
@@ -10,12 +10,43 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  public project = {
+    type: 'Proje',
+    image: 'assets/img/music_project.png',
+    title: 'Music Player Concept',
+    content: 'Music player application concept. Made with Figma and Photoshop.',
+    parent : {
+      image: 'assets/img/alekted.png',
+      name : 'ALECTED'
+    },
+  };
+
+  public project2 =  {
+    image: 'assets/img/placeholder.png',
+    title: 'AlecTED Otonom Araç Projesi',
+    parent : {
+      image: 'assets/img/alekted.png',
+      name : 'ALECTED'
+    },
+    content : 'ALECTED topluluğu olarak otonom araç projesi üzerinde çalışıyoruz'
+  }
+
+  
+
+  public post = {
+    type: 'Gönderi',
+    image: 'assets/img/alekted_post.png',
+    title: 'ALECTED TAKIMIMIZ BAŞARILARIYLA ÜLKEMİZİ TEMSİL EDİYOR!',
+    content:
+      'Shell’in düzenlemiş olduğu “Tracking and Reducing CO2 Emissions from Vehicles” konulu yarışmada, Üniversitemiz öğrencilerinin oluşturmuş olduğu AlecTED’in, Avrupa - Afrika bölgelerinde 1. olduğunu ve tek Türk takım olarak dünya finalinde Ülkemizi, bölgemizi ve Üniversitemizi temsil edeceğini duyurmaktan gurur duyuyoruz.',
+  };
+
   public user = {
     name: 'Rıfkı yenidoğan',
     job: 'TED Üniversitesi - Yazılım Mühendisliği',
     info: '500+ Bağlantı • Ankara, Türkiye',
     avatar: 'assets/img/profile-avatar.jpeg',
-    backdrop : 'assets/img/profile-background.png',
+    backdrop: 'assets/img/profile-background.png',
     organizations: [
       {
         name: 'TED Üniversitesi',
@@ -34,8 +65,8 @@ export class UserProfileComponent implements OnInit {
       website: 'www.example.com',
       address: 'example address',
     },
-    posts: [{}],
-    featured: [],
+    posts: [this.post, this.post],
+    featured: [this.project, this.post],
     experience: [
       {
         title: 'Software Engineer',
@@ -58,31 +89,32 @@ export class UserProfileComponent implements OnInit {
             level: 80,
           },
         ],
+        featured: [this.project, this.post],
       },
     ],
     education: [
       {
         title: 'Yazılım Mühendisliği',
         school: 'TED Üniversitesi',
-        image : 'assets/img/ted.png',
+        image: 'assets/img/ted.png',
         date: '2017 - 2021',
         description:
           'Lorem ipsum dolor sit amet consectetur. Consectetur dictum auctor nec morbi diam. Maecenas proin porttitor enim viverra feugiat quam felis aliquam.',
-        communuties: [
+        communities: [
           {
-            name: 'ALECED',
-            image: '',
+            name: 'ALECTED',
+            image: 'assets/img/alekted.png',
             role: 'Üye',
             joinDate: '2020 - 2021',
           },
         ],
-        talents : [
+        talents: [
           {
             name: 'HTML',
             level: 90,
-          }
+          },
         ],
-        educationInfo : {
+        educationInfo: {
           type: 'Lisans',
           startYear: '2017',
           endYear: '2021',
@@ -91,33 +123,41 @@ export class UserProfileComponent implements OnInit {
           minor: 'Bilgisayar Mühendisliği',
           school: 'TED Üniversitesi',
           schoolType: 'Üniversite',
+          elective: [
+            { name: 'Oyun Programlama', grade: 'AA' },
+            { name: 'Algoritma Analizi', grade: 'BA' },
+            { name: 'Mikroişlemciler', grade: 'CB' },
+          ],
         },
-        featured : [
-        ],
-
+        featured: [this.post, this.project],
       },
     ],
     projects: {
-      completed : [
-      ],
-      inProgress : []
+      completed: [this.project],
+      inProgress: [this.project2,this.project],
     },
     certificates: [
       {
-        name : "",
-        image : "",
-        date : "",
-        id : "",
-        accomplishments : [
+        name: 'Complete Web & Mobile Designer in 2022: UI/UX, Figma + more',
+        platform : 'Zero To Mastery Academy',
+        image: 'assets/img/cert.png',
+        date: 'Eylül 2022',
+        id: 'UC-b0047967-fe9c-46ac-918b-707070707070',
+        accomplishments: [
+          {
+            name: 'HTML',
+            level: 90,
+          },
+          {
+            name: 'CSS',
+            level: 80,
+          },
         ],
-
-      }
+        featured: [this.project, this.post],
+      },
     ],
-    communities : [
-    ],
-    talents : [
-    ],
-    interests : [
-    ],
+    communities: [],
+    talents: [],
+    interests: [],
   };
 }
