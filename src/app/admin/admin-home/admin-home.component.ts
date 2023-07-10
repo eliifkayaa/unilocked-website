47330c052fb505a8ca2fb2b35f658442b5b30632
CaptainSP <19982002ke@gmail.com>
2023-07-10 22:41:25 +0300

    Admin home

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import MenuItem from '../models/menu-item';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { Chart } from 'chart.js';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'admin-home',
@@ -9,20 +15,25 @@ import { Chart } from 'chart.js';
   styleUrls: ['./admin-home.component.scss'],
   animations: [
     trigger('buttonState', [
-      state('active', style({
-        backgroundColor: 'FBFBFC',
-        color: '#495057'
-      })),
-      state('inactive', style({
-        background: "none",
-        color: '#F1F3F5'
-      })),
-      transition('inactive => active', animate('500ms ease-in'))
-    ])
-  ]
+      state(
+        'active',
+        style({
+          backgroundColor: 'FBFBFC',
+          color: '#495057',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          background: 'none',
+          color: '#F1F3F5',
+        })
+      ),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
 export class AdminHomeComponent implements OnInit {
-
   public menuItems: MenuItem[][] = [
     [
       {
@@ -30,21 +41,21 @@ export class AdminHomeComponent implements OnInit {
         icon: 'home',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: false
+        expands: false,
       },
       {
         label: 'İstatistikler',
         icon: 'query_stats',
         route: '/admin/statistics',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
       {
         label: 'Profil',
         icon: 'grid_on',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
     ],
     [
@@ -53,28 +64,28 @@ export class AdminHomeComponent implements OnInit {
         icon: 'people_outline',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
       {
         label: 'Projeler',
         icon: 'rocket_launch',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
       {
         label: 'Etkinlikler',
         icon: 'event',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
       {
         label: 'İlanlar',
         icon: 'newspaper',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: true
+        expands: true,
       },
     ],
     [
@@ -83,73 +94,89 @@ export class AdminHomeComponent implements OnInit {
         icon: 'verified',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: false
+        expands: false,
       },
       {
         label: 'İş Birliği Platformu',
         icon: 'group_work',
         route: '/admin/dashboard',
         roles: ['admin'],
-        expands: false
+        expands: false,
       },
     ],
     [
-    {
-      label: 'Mesajlaşma',
-      icon: 'chat',
-      route: '/admin/dashboard',
-      roles: ['admin'],
-      expands: false
-    },
-    {
-      label: 'Bildirim',
-      icon: 'notifications',
-      route: '/admin/dashboard',
-      roles: ['admin'],
-      expands: true
-    },
-    ]
+      {
+        label: 'Mesajlaşma',
+        icon: 'chat',
+        route: '/admin/dashboard',
+        roles: ['admin'],
+        expands: false,
+      },
+      {
+        label: 'Bildirim',
+        icon: 'notifications',
+        route: '/admin/dashboard',
+        roles: ['admin'],
+        expands: true,
+      },
+    ],
   ];
 
-  activeBtn = "btn1"
+  activeBtn = 'btn1';
   public setActive = (btnName) => {
-    this.activeBtn = btnName
-  }
+    this.activeBtn = btnName;
+  };
 
   constructor() {}
-  ngOnInit() : void{}
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.ctx = this.canvas1.nativeElement.getContext('2d');
 
-  canvas : any;
-  ctx: any;
-  @ViewChild('mychart') mychart:any;
+      const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+      const data = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)',
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+      // </block:setup>
 
-  ngAfterViewInit() {
-    this.canvas = this.mychart.nativeElement;
-    this.ctx = this.canvas.getContext('2d');
+      // <block:config:0>
+      const config = {
+        type: 'bar',
+        data: data,
+      };
 
-    new Chart(this.ctx, {
-        type: 'line',
-        data: {
-            datasets: [{
-                label: 'Current Vallue',
-                data: [0, 20, 40, 50],
-                backgroundColor: "rgb(115 185 243 / 65%)",
-                borderColor: "#007ee7",
-                fill: true,
-            },
-            {
-                label: 'Invested Amount',
-                data: [0, 20, 40, 60, 80],
-                backgroundColor: "#47a0e8",
-                borderColor: "#007ee7",
-                fill: true,
-            }],
-            labels: ['January 2019', 'February 2019', 'March 2019', 'April 2019']
-        },
-    });
+      this.chart = new Chart(this.ctx, config as any);
+    }, 1000);
   }
 
+  ctx: any;
+  public chart: any;
 
-  
+  @ViewChild('canvas1') canvas1: any;
 
+  ngAfterViewInit() {}
 }
