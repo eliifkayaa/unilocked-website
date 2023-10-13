88dd42d088a02532a20feb8542a6de33a18afeb0
haslyk <halimaslyk77@gmail.com>
2023-10-13 17:40:44 +0300

    Discovery Catalog Created

---
+++ src/app/site/discovery-catalog/catalog-cards/intern-card/intern-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intern-card',
+  templateUrl: './intern-card.component.html',
+  styleUrls: ['./intern-card.component.scss']
+})
+export class InternCardComponent {
+
+}
