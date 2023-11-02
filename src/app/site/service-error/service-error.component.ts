bc30b60f87796c3254dca8471e31ab44a4dc4aa0
Eren Kaan ÇAKIR <eren.cakir@tedu.edu.tr>
2023-11-02 18:03:12 +0300

    error pages added

---
+++ src/app/site/service-error/service-error.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'service-error',
+  templateUrl: './service-error.component.html',
+  styleUrls: ['./service-error.component.scss']
+})
+export class ServiceErrorComponent {
+
+}
